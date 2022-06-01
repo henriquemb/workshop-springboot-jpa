f02c5b3720bc5f71ce0f71398570d7e03e5c1716
Moacir Henrique Morais Baruffi <moacirhenrique01@hotmail.com>
2022-06-01 10:09:17 -0300

    Project created

---
+++ src/test/java/com/github/henriquemb/workshopspringbootjpa/WorkshopSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.henriquemb.workshopspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
