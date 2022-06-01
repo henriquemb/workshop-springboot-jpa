ccfba337aa71e939a4eb4e406d3b50b9132cad12
Moacir Henrique Morais Baruffi <moacirhenrique01@hotmail.com>
2022-06-01 10:49:01 -0300

    H2 database, test profile, JPA

--- src/main/java/com/github/henriquemb/workshopspringbootjpa/entities/User.java
+++ src/main/java/com/github/henriquemb/workshopspringbootjpa/entities/User.java
@@ -1,11 +1,16 @@
 package com.github.henriquemb.workshopspringbootjpa.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "`user`")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
